549d9ead64b631ea221d12c87d554f15ac7c6d1a
Nidhi260 <singh1230nidhi@gmail.com>
2025-07-19 16:33:40 +0530

    More practice on loops

---
+++ Loops/Alternate_Sum.java
@@ -0,0 +1,21 @@
+package Loops;
+import java.util.Scanner;
+public class Alternate_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            int ans = 0;
+            for(int i =1; i<=n; i++){
+                if(i % 2 ==0){
+                    ans -= i;
+                }else{
+                    ans += i;
+                }
+            }
+            
+        System.out.println(ans);
+        sc.close();
+    }
+
+}
---
+++ Loops/SumOfDigits.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.Scanner;
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner  sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int original_n = n;
+
+        while(n >0){
+            sum += n % 10;
+            n /= 10;  
+        }
+        System.out.println("Sum of digits of: " + original_n + " = " + sum);
+        sc.close();
+    }
+
+}
