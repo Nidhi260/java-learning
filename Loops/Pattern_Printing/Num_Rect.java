ed5e7f92ea9e87020792cfeafe4fca36094134c2
Nidhi260 <singh1230nidhi@gmail.com>
2025-07-23 00:09:25 +0530

    Number and star pattern learned

--- Loops/Pattern_Printing/Num_Rect.java
+++ Loops/Pattern_Printing/Num_Rect.java
@@ -1,18 +1,21 @@
 package Loops.Pattern_Printing;
 import java.util.Scanner;
-public class Tri_star {
+public class Num_Rect {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
         int r = sc.nextInt();
 
-        for(int i =r; i>=1; i--){
-            for (int j = 1; j<=i; j++) {
-                System.out.print("*");
+        for(int i=1; i<=r; i++){
+            for(int j = i; j<=r; j++){
+                System.out.print(j);
+            }
+            for (int k = 1; k <= i-1; k++) {
+                System.out.print(k);
             }
             System.out.println();
         }
         sc.close();
+
     }
 
 }
