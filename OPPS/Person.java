ef9e4ae15820b9a99a9207f7ac56af5c427c9e20
Nidhi260 <singh1230nidhi@gmail.com>
2025-07-26 22:03:08 +0530

    Learned classes and objects

---
+++ OPPS/Person.java
@@ -0,0 +1,11 @@
+package OPPS;
+
+public class Person {
+    int age = 22;
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.age);
+        
+    }
+    
+}
---
+++ OPPS/Student.java
@@ -0,0 +1,13 @@
+package OPPS;
+
+public class Student {
+    int age;
+    String studentName;
+
+    public static void main(String[] args) {
+        Student obj = new Student();
+        System.out.println(obj.age);
+        System.out.println(obj.studentName);
+    }
+
+}
