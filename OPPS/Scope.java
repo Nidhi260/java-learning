a6f72cf7603bc91d857559827a68cec9e2966444
Nidhi260 <singh1230nidhi@gmail.com>
2025-07-27 23:24:50 +0530

    Learned about scope of  variables

---
+++ OPPS/Scope.java
@@ -0,0 +1,25 @@
+package OPPS;
+
+class Algebra{
+    int a = 10;  //member variable 
+    int b = 5;
+
+    int add(){
+        return a+b;
+    }
+
+    int sub(){
+        return a-b;
+    }
+    void demo(){
+        int c = 50;
+        System.out.println(c);
+    }
+}
+
+public class Scope {
+    public static void main(String[] args) {
+        
+    }
+
+}
