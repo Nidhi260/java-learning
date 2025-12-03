c0cade1ad06bee68473e00176f0acfd633c8d437
Nidhi260 <singh1230nidhi@gmail.com>
2025-12-03 23:52:03 +0530

    Revised the basics of operators and its precedence

---
+++ variable_java/Expression.java
@@ -0,0 +1,9 @@
+public class Expression {
+    public static void main(String[] args) {
+        // float a = 7/4.0f * 9/2.0f;
+       // int x = 7;
+        int a = 7*49/7 +35/7;
+        System.out.println(a);
+    }
+
+}
---
+++ variable_java/Phy_expression.java
@@ -0,0 +1,8 @@
+public class Phy_expression {
+    public static void main(String[] args) {
+    
+        System.out.println("(v² - u²)/2as");
+
+    }
+
+}
