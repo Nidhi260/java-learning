46abf0626c8712932db92ad70883911597c16795
Nidhi260 <singh1230nidhi@gmail.com>
2025-12-10 15:37:52 +0530

    Learned the built in methods of the String

---
+++ Strings/intro.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class intro {
+    public static void main(String[] args) {
+        String name = "Harry";
+        int value = name.length();
+        System.out.println(value);
+
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
+        
+        // System.out.print("The name is : ");
+        // System.out.println(name);
+        // int a = 6;
+        // float b =  5.645f;
+        // System.out.printf("The value of a is %d and value of b is %f" ,a ,b);
+
+
+    }
+
+}
