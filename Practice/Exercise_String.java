f2cd7e0c9d7efa58197327f729ade8abd97f9010
Nidhi260 <singh1230nidhi@gmail.com>
2025-12-10 16:08:26 +0530

    Practiced some questions of string basic

---
+++ Practice/Exercise_String.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class Exercise_String {
+    public static void main(String[] args) {
+        //Problem 1 :
+        // String name = "Tony Stark";
+        // System.out.println(name.toLowerCase());
+
+        //Problem 2:
+        // String text = "To  Lower   Case";
+        // text = text.replace(" ", "_");
+        // System.out.println(text);
+
+        //Problem 3:
+        // String letter ="Dear <|name|>, Thanks a lot!";
+        // letter = letter.replace("<|name|>", "Nidhi");
+        // System.out.println(letter);
+
+        //Probblem 4:
+        // String myString = "This string contains  double and triple places";
+        // System.out.println(myString.indexOf("  "));
+        // System.out.println(myString.indexOf("   "));
+
+        String myletter = "Dear Nidhi ,\n\tThis java course is nice.\n\tThanks!";
+        System.out.println(myletter);
+
+    }
+
+}
